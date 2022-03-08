1bea025a7596df2776a6101d9ed65c19f95fa5bf
Azhagarasan2ar <azhagarasan777@gmail.com>
2022-03-08 06:26:10 -0800

    adding file

---
+++ Char.java
@@ -0,0 +1,15 @@
+package com.stringprog;
+
+public class Char {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String word="jumbalaka";
+		System.out.println(word.contains("a"));
+		
+		word=word.replace("a", "");
+		System.out.println(word);
+	}
+
+}
